39efce66bfe76e3fc387a9560c8980add8ce5d73
BuildTools <unconfigured@null.spigotmc.org>
2024-11-29 16:02:19 +0100

    Initial Commit

---
+++ src/main/java/gg/kpjm/transientDevTools/TransientDevTools.java
@@ -0,0 +1,17 @@
+package gg.kpjm.transientDevTools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TransientDevTools extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logicss
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
